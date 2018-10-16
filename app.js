14a0388db8d0164fb8aad97a49f624bdf4bb17c1
Mayank Gupta <techfreakworm@gmail.com>
2018-10-16 13:19:42 +0530

    Add new route, protect clean route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 let trainDescriptorsByClass = []
 const app = express()
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -19,11 +19,18 @@ if (env == "development") {
     process.env.PORT = 2018;
 }
 console.log("app live on :", process.env.PORT)
-app.get('/api/:uid', (req, res) => {
+app.get('/api/clean/:pass', (req, res) => {
 
-    res.send("Hello " + trainDescriptorsByClass);
-    res.status(200);
-    trainDescriptorsByClass = [];
+    if (req.params.pass == '121002') {
+        trainDescriptorsByClass = [];
+        res.status(200);
+        res.send("Model cleaned");
+    }
+
+    else {
+        res.status(400)
+        res.send("Model not cleaned, invalid password");
+    }
 })
 
 app.post('/api', (req, res) => {
@@ -32,8 +39,12 @@ app.post('/api', (req, res) => {
         "className": req.body.className
     }
 
-
     trainDescriptorsByClass.push(descriptorByClass)
+    res.status(200)
+    res.send(trainDescriptorsByClass)
+})
+
+app.get('/api/getModel', (req, res) => {
     res.status(200)
     res.send(trainDescriptorsByClass)
 })
